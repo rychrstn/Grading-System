dd4a69d4dff81accc6c63f8560d8e831cd1ba3a7
Anthony <anthonyyuvega09@gmail.com>
2021-10-13 23:38:41 +0800

    student login and student register with password encryption and password hash

--- student_login.php
+++ student_login.php
@@ -8,8 +8,20 @@
 </head>
 <body>
     <?php
+    include ('connection.php');
+    $Select = "SELECT * FROM `Students`";
+    $sql = mysqli_query($conn,$Select);
+    if($rows = mysqli_num_rows($sql)> 0 ){
+        while($rows = mysqli_fetch_array($sql)){
+            $id = $rows['Valid'];
     ?>
     <form action="" method="POST">
+        <input type="hidden" name="bool" value="<?php echo $id ?>">
+        <?php
+        }
+    }
+
+    ?>
         <label>Username</label>
         <input type="text" name="username" placeholder="Enter Username">
         <br>
@@ -18,29 +30,48 @@
         <br>
         <input type="submit" name="login" value="Login">
     </form>
-    
+   
 </body>
 </html>
 <?php
-require ('connection.php');
+include ('connection.php');
 if(isset($_POST['login'])){
     $Username = mysqli_real_escape_string($conn,$_POST['username']);
     $Password = mysqli_real_escape_string($conn,$_POST['password']);
+    $Password = password_hash($Password, PASSWORD_DEFAULT);
+    $bool = $_POST['bool'];
+
+
 
     if(empty($Username) || empty($Password)){
         echo "<script>alert('empty input fields');</script>";
 
     }else{
         $Query = "SELECT * FROM `Students` WHERE Username = '$Username' AND Password = '$Password'";
-        $sql = mysqli_query($conn, $Query);
-        if(mysqli_num_rows($sql) > 0 ){
-            echo "hellos";
-            header('Location:index.html');
-        }else{
-            echo "Error".$sql."<br>".$conn->error;
+        $Sql = mysqli_query($conn,$Query);
 
+        if(mysqli_num_rows($sql)> 0)
+        {
+            if($bool == 1 )
+            {
+                echo "hello";
+            }
+            else{
+                echo "Your account is not yet validated by the office";
+            }
+            while($row = mysqli_fetch_assoc($sql))
+            {
+                if(password_verify($Passowrd,$row['password']))
+                {
+                    echo "right password";
+                }
+            }
+            
+        }else{
+            echo "error".$sql."<br>".$conn->error;
         }
     }
 }
 
+mysqli_close($conn);
 ?>
