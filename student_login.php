cfd965142d2cd9131187801cce62339e1ea74a20
rychrstn <raywhuncarinp@gmail.com>
2021-10-17 23:03:52 +0800

    Backend wrong pw/validation

--- student_login.php
+++ student_login.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Login</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
-<body>
+</html>
     <?php
     include ('connection.php');
     $Select = "SELECT * FROM `students`";
@@ -15,14 +23,40 @@
     }
 
     ?>
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Enter Username">
+        <section>
+    <div class="box"></div>
+        </section>
+    <form action="" method="POST">
+    <div class="textboxdiv">
+    <div class="title"><h1>The Univerity of Manila</h1>
+    <img class="um_logo" src="images/um.jpeg" alt="umlogo" height="120" width="140"></a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div>
+        <input style="border: 2px solid lightgreen; font-size: 20px"
+        type="text" name="username" placeholder="Username">
         <br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Enter Password">
         <br>
-        <input type="submit" name="login" value="Login">
+        <input style="border: 2px solid lightgreen; font-size: 20px"
+         type = "password" name="password" placeholder="Password">
+        <br>
+        <br>
+        <br>
+        <input class="btnlogin" style="width: 100px; height: 50px; color: white; background-color:black; font-size: 20px;" type="submit" name="login" value="Login">
+        <div class="signup">
+			Don't have an Account?
+			</br>
+			<a style = "color: #1EA4E3"; href="register.php">Click here to Sign up</a>
+		</div>
+        </div>
+    </div>
     </form>
+
+
+</body>
+</html>
     
 
 </body>
@@ -46,10 +80,10 @@ if(isset($_POST['login'])){
                         if($Bool == 1){
                             header('location:index.php');
                         }else{
-                        echo "You're not yet validated";
+                        echo "<div class = FieldError><p>You're not yet validated</p></div>";
                         }
                         }else{
-                            echo "wrong password";
+                        echo "<div class = FieldError1><p>Wrong Password!</p></div>";
                         }
             }else{
                 echo "no account".$Sql."<br>".$conn->error;
