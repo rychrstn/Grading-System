9ff652e335d99d7880ca2aff266a85a66b9d038a
Anthony <anthonyyuvega09@gmail.com>
2021-11-06 11:01:55 +0800

    grades  of student in the index page

--- index.php
+++ index.php
@@ -41,7 +41,45 @@ if(!isset($_SESSION['username'], $_SESSION['id'], $_SESSION['studentid'])){
     <form action="" method="POST">
         <input type="submit" name="students" value="show grade">
     </form>
-    
-    
+    <table>
+        <thead>
+            <tr>
+                <th>Professor Name</th>
+                <th>Subject</th>
+                <th>Grades</th>
+                <th>Term</th>
+                <th>Remarks</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            include('connection.php');
+            $SelectStudent = "SELECT  a.StudentID,a.YearAndCourse, concat(a.Firstname, ' ', a.Middlename, ' ', a.Lastname) as Student,b.Grades, b.Term,c.SubjectCode, c.SubjectName,concat(d.Firstname, ' ', d.Middlename, ' ', d.Lastname) as Professor
+            FROM
+            students AS a
+                JOIN
+            grades AS b ON b.Student_ID = a.ID
+                JOIN
+            subjects c ON c.id = b.Subject_ID
+                JOIN
+            professor AS  d ON d.id = b.Prof_ID
+
+            WHERE b.Student_ID= '".$_SESSION['id']."'";
+             $Run = mysqli_query($conn, $SelectStudent);
+             while($Row = mysqli_fetch_assoc($Run)){
+            ?>
+            <tr>
+                <td><?php echo $Row['Professor'];?></td>
+                <td><?php echo $Row['SubjectCode'];?></td>
+                <td><? echo $Row['Term']?></td>
+                <td><?php echo $Row['Grades'];?></td>
+            </tr>
+            <?php
+             }
+            
+
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
