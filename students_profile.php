e54f021b7d306667f67bf495e5656cac79935d79
rychrstn <raywhuncarinp@gmail.com>
2021-11-18 16:58:35 +0800

    update

--- students_profile.php
+++ students_profile.php
@@ -11,9 +11,18 @@ if(!isset($_SESSION['username'], $_SESSION['id'], $_SESSION['studentid'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/students_profile.css">
+    <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <title>Student's Profile</title>
 </head>
-<body>
+
     <?php
     include('connection.php');
     $Selectstudents = " SELECT a.ID, a.StudentID,a.YearAndCourse, concat(a.Firstname, ' ', a.Middlename, ' ', a.Lastname) as Student, a.Username,a.Password,a.ContactNumber,a.StudentStatus,a.DateTimeUpdated FROM students AS a WHERE ID = '".$_SESSION['id']."'";
@@ -21,7 +30,8 @@ if(!isset($_SESSION['username'], $_SESSION['id'], $_SESSION['studentid'])){
     if(mysqli_num_rows($Sql) > 0 ) { 
         foreach($Sql AS $Rows){
             ?>
-            <table>
+<body> 
+<table class="profile">
                 <thead>
                     <th>Student ID </th>
                     <th>Student's Name</th>
@@ -46,6 +56,8 @@ if(!isset($_SESSION['username'], $_SESSION['id'], $_SESSION['studentid'])){
                 </tbody>
             </table>
 
+            
+            
 
 
             <?php
