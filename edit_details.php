e31d3a9c1bf3a87b2d5e9e6d65e88b38abbd27ad
rychrstn <raywhuncarinp@gmail.com>
2021-11-18 19:45:35 +0800

    update

--- edit_details.php
+++ edit_details.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <?php
-   
+
     include ('connection.php');
     if(isset($_GET['edit'])){
         $id = $_GET['edit_detail'];
@@ -18,49 +18,71 @@
         if(mysqli_num_rows($Sql)> 0 ){
             foreach($Sql as $results){
                 ?>
-               <form action="update_details.php" method="POST">
+                <div class="container">
+                    <div class="title">Update Information</div>
+                <form action="update_details.php" method="POST">
                 <input type="hidden" name="edit_id" value="<?php echo $id?>">
-                <label> Username </label>
-                <input  type="text" name="editusername"  maxlength="20" value="<?php echo $results['Username'];?>">
+                <div class="user-details">
+                <div class="input-box">
+                <span class="details">Username</span>
+                <input class="input-box"  type="text" name="editusername"  maxlength="20" value="<?php echo $results['Username'];?>">
                 <p><?php if(isset($errors['editusername'])) echo $errros['editusername']?></p>
-                <br>
-                <label> Password </label>
+                </div>
+                <div class="input-box">
+                <span class="details">Password</span>
                 <input type="password" name="editpassword"  maxlength="20" value="<?php  $results['Password'];?>">
-                <br>
-                <label> Student ID</label>
+                </div>
+                <div class="input-box">
+                <span class="details">Student ID</span>
                 <input type="text" name="editid"  maxlength="7" value="<?php echo $results['StudentID'];?>">
-                <br>
-                <label> Firstname</label>
+                
+                </div>
+                <div class="input-box">
+                <span class="details">Firstname</span>
                 <input type="text" name="editfirstname"  maxlength="20" value="<?php echo $results['Firstname']?>">
-                <br>
-                <label> Middlename </label>
+                
+                </div>
+                <div class="input-box">
+                <span class="details">Middlename</span>
                 <input type="text" name="editmiddlename"  maxlength="20" value="<?php echo $results['Middlename']?>">
-                <br>
-                <label> Lastname</label>
+                
+                </div>
+                <div class="input-box">
+                <span class="details">Lastname</span>
                 <input type="text" name="editlastname"  maxlength="20" value="<?php echo $results['Lastname']?>">
-                <br>
-                <label> Year & Ccourse </label>
+                
+                </div>
+                <div class="input-box">
+                <span class="details">Year & Ccourse</span>
                 <input type="text" name="edityearcourse" maxlength="19"  value="<?php echo $results['YearAndCourse']?>">
-                <br>
-                <label> Contact Number</label>
+                
+                </div>
+                <div class="input-box">
+                <span class="details">Contact Number</span>
                 <input type="text" name="editnumber"  maxlength="11"  onkeypress="return isNumber(event)" value="<?php echo $results['ContactNumber']?>">
+                </div>
+                <div class="input-box">
+                <span class="student-details">Student Status</span>
                 <br>
-                <label> Student Status</label>
-                <select name="editstudentstatus">
+                <select class="select" name="editstudentstatus">
                 <option value="Regular"<?php if ($results['StudentStatus'] === 'Regular') echo ' selected="selected"'?>>Regular</option>
                 <option value="Irregular"<?php if ($results['StudentStatus'] === 'Irregular') echo ' selected="selected"'?>>Irregular</option>
-                    <?php
+                </div>
+                </div>
+                </div>
                 
+                    <?php
+                    
                     if($results['StudentStatus'] == 'Regular'){
                         echo "Selected";
-                      
+                    
                     }else if($results['StudentStatus'] == 'Irregular'){
                         echo "Selected";
                     }
                     ?>
                 </select>
                 <br>
-                <input type="submit" name="update_students" value="Update Students">
+                <input class="update-btn" type="submit" name="update_students" value="Update">
                 <?php
 
             }
--- students_profile.php
+++ students_profile.php
@@ -30,36 +30,70 @@ if(!isset($_SESSION['username'], $_SESSION['id'], $_SESSION['studentid'])){
     if(mysqli_num_rows($Sql) > 0 ) { 
         foreach($Sql AS $Rows){
             ?>
-<body> 
-<table class="profile">
-                <thead>
-                    <th>Student ID </th>
-                    <th>Student's Name</th>
-                    <th>Username</th>
-                    <th>Year And Course</th>
-                    <th>Contact Number </th>
-                    <th> Student Status</th>
-                </thead>
-                <tbody>
-                    <td><?php echo $Rows['StudentID'];?></td>
-                    <td><?php echo $Rows['Student'];?></td>
-                    <td><?php echo $Rows['Username'];?></td>
-                    <td><?php echo $Rows['YearAndCourse']?></td>
-                    <td><?php echo $Rows['ContactNumber']?></td>
-                    <td><?php echo $Rows['StudentStatus'];?></td>
-                    <td>
-                        <form action="edit_details.php" method="GET">
+<body class="bg-light"> 
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10 mt-5 pt-5">
+                <div class="row z-depth-3">
+                    <div class="col-sm-4 bg-info rounded-left">
+                        <div class="card-block text-center text-white">
+                            <i class="far fa-user-circle fa-7x mt-5"></i>
+                            <h2 class="font-weight-bold mt-4"><td><?php echo $Rows['Student'];?></td></h2>
+                            <p>Student</p>
+                            <form action="edit_details.php" method="GET">
                             <input type="hidden" name="edit_detail" value="<?php echo $Rows['ID']?>">
                             <input type="submit" name="edit" value="Edit Profile">
                         </form>
-                    </td>
-                </tbody>
-            </table>
-
-            
-            
-
-
+                        </div>
+                    </div>
+                    <div class="col-sm-8 bg-white rounded-right">
+                        <h3 class="mt-3 text-center">Information</h3>
+                        <hr class="badge-success mt-0 w-25" style="position: absolute; left: 38%">
+                        <br>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="font-weight-bold">Student ID</p>
+                                <h6 class="text-muted"><td><?php echo $Rows['StudentID'];?></td></h6>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="font-weight-bold">Username</p>
+                                <h6 class="text-muted"><td><?php echo $Rows['Username'];?></td></h6>
+                            </div>
+                        </div>
+                        <h3 class="mt-3"></h3>
+                        <hr class="bg-success">
+                        <div class="row">
+                        <div class="col-sm-6">
+                                <p class="font-weight-bold">Year And Course</p>
+                                <h6 class="text-muted"><td><?php echo $Rows['YearAndCourse']?></td></h6>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="font-weight-bold">Contact Number</p>
+                                <h6 class="text-muted"><td><td><?php echo $Rows['ContactNumber']?></td></h6>
+                            </div>
+                        </div>
+                        <h3 class="mt-3"></h3>
+                        <hr class="bg-success">
+                        <div class="row">
+                        <div class="col-sm-6">
+                                <p class="font-weight-bold">Student Status</p>
+                                <h6 class="text-muted"><td><?php echo $Rows['StudentStatus'];?></td></h6>
+                                <div class="wrapper">
+                                <div class="link_wrapper">
+                                <a href="index.php">Dashboard</a>
+                                <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
+                                <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
+                                </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
             <?php
 
         }
